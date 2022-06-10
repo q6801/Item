bcc01322ae74520f0e5652bb4c8b2291b885b3ba
Lee SeungHun <q6801@naver.com>
2022-06-10 10:30:10 +0900

    test: itemService의 setUserDto test 작성

--- src/main/java/com/ssg/item/dto/ItemDto.java
+++ src/main/java/com/ssg/item/dto/ItemDto.java
@@ -1,21 +1,38 @@
 package com.ssg.item.dto;
 
 import com.ssg.item.enums.ItemType;
+import com.ssg.item.exception.CustomRuntimeException;
+import com.ssg.item.exception.ExceptionEnum;
 import lombok.Getter;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @Getter
 public class ItemDto {
-    @NotBlank
+    @NotBlank(message = "상품의 이름은 빈값일 수 없습니다.")
     private String name;
-    @NotNull
+    @NotNull(message = "상품의 타입은 널값일 수 없습니다.")
     private ItemType itemType;
+    @Min(value = 0, message = "상품의 비용은 음수일 수 없습니다.")
     private int itemPrice;
-    @NotNull
+    @NotNull(message = "상품 전시 시작기간은 널값일 수 없습니다.")
     private Timestamp itemDisplayStartDate;
-    @NotNull
+    @NotNull(message = "상품 전시 종료기간은 널값일 수 없습니다.")
     private Timestamp itemDisplayEndDate;
+
+    public ItemDto(String name, ItemType itemType, int itemPrice, Timestamp itemDisplayStartDate, Timestamp itemDisplayEndDate) {
+        this.name = name;
+        this.itemType = itemType;
+        this.itemPrice = itemPrice;
+        this.itemDisplayStartDate = itemDisplayStartDate;
+        this.itemDisplayEndDate = itemDisplayEndDate;
+
+        if (itemDisplayStartDate!=null && itemDisplayEndDate!=null &&
+                itemDisplayStartDate.after(itemDisplayEndDate)) {
+            throw new CustomRuntimeException(ExceptionEnum.BAD_ITEM_DISPLAY_TIME);
+        }
+    }
 }
---
+++ src/test/java/com/ssg/item/service/ItemServiceTest.java
@@ -0,0 +1,109 @@
+package com.ssg.item.service;
+
+import com.ssg.item.dto.ItemDto;
+import com.ssg.item.dto.ItemResDto;
+import com.ssg.item.entity.Item;
+import com.ssg.item.enums.ItemType;
+import com.ssg.item.exception.CustomRuntimeException;
+import com.ssg.item.repository.ItemRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemServiceTest {
+    private static Validator validator;
+
+    @InjectMocks
+    private ItemServiceImpl itemService;
+
+    @Mock
+    private ItemRepository itemRepository;
+
+    @BeforeAll
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+
+    @Test
+    @DisplayName("setUserDto 성공")
+    public void setItem() {
+        Item item = getStubItems().get(0);
+        ItemDto inputData = new ItemDto(item.getName(), item.getItemType(), item.getItemPrice(),
+                item.getItemDisplayStartDate(), item.getItemDisplayEndDate());
+
+        given(itemRepository.save(item)).willReturn(item);
+        ItemResDto itemResDto = itemService.setItem(inputData);
+
+        assertThat(itemResDto.getName()).isEqualTo(item.getName());
+        assertThat(itemResDto.getItemType()).isEqualTo(item.getItemType());
+        assertThat(itemResDto.getItemPrice()).isEqualTo(item.getItemPrice());
+        assertThat(itemResDto.getItemDisplayEndDate()).isEqualTo(item.getItemDisplayEndDate());
+        assertThat(itemResDto.getItemDisplayStartDate()).isEqualTo(item.getItemDisplayStartDate());
+    }
+
+    @Test
+    @DisplayName("item의 display 시작시간이 종료시간보다 늦거나 같은 경우")
+    public void setItemWithWrongDate() {
+        String itemName = "item name";
+        ItemType itemType = ItemType.NORMAL_ITEM;
+        int itemPrice = 100;
+        Timestamp first = Timestamp.valueOf(LocalDateTime.now().withNano(0));
+        Timestamp second = Timestamp.valueOf(LocalDateTime.now().plusMonths(1).withNano(0));
+
+        Assertions.assertThatThrownBy(() -> new Item(itemName, itemType, itemPrice, second, first))
+                .isInstanceOf(CustomRuntimeException.class);
+
+        Assertions.assertThatThrownBy(() -> new ItemDto(itemName, itemType, itemPrice, second, first))
+                .isInstanceOf(CustomRuntimeException.class);
+
+        new ItemDto(itemName, itemType, itemPrice, first, second);
+        new ItemDto(itemName, itemType, itemPrice, first, first);
+        new ItemDto(itemName, itemType, itemPrice, first, second);
+        new ItemDto(itemName, itemType, itemPrice, first, first);
+    }
+
+    @Test
+    @DisplayName("itemDto에 잘못된 입력이 들어간 경우")
+    public void setItemWithWrongInput() {
+        ItemDto itemDto = new ItemDto("", null, -123, null, null);
+
+        Set<ConstraintViolation<ItemDto>> validate = validator.validate(itemDto);
+        assertThat(validate.size()).isEqualTo(5);
+        for(ConstraintViolation<ItemDto> constraintViolation : validate){
+            System.out.println(constraintViolation.getMessage());
+        }
+    }
+
+    private List<Item> getStubItems() {
+        Timestamp time0 = Timestamp.valueOf(LocalDateTime.now().withNano(0));
+        Timestamp time1 = Timestamp.valueOf(LocalDateTime.now().plusMonths(1).withNano(0));
+        Timestamp time2 = Timestamp.valueOf(LocalDateTime.now().plusMonths(2).withNano(0));
+        Timestamp time3 = Timestamp.valueOf(LocalDateTime.now().plusMonths(2).withNano(0));
+
+        List<Item> items = new ArrayList<>();
+        items.add(new Item(0, "name0", ItemType.NORMAL_ITEM, 100, time0, time1));
+        items.add(new Item(1, "name1", ItemType.CORPORATE_USER_ITEM, 200, time2, time3));
+        return items;
+    }
+}
