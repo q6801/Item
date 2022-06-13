1126191f19a7c2f3c621df36a78040e82afc0793
Lee SeungHun <q6801@naver.com>
2022-06-13 10:42:38 +0900

    test: 상품과 그 상품에 해당하는 프로모션 조회 test

--- src/test/java/com/ssg/item/service/ItemServiceTest.java
+++ src/test/java/com/ssg/item/service/ItemServiceTest.java
@@ -2,7 +2,10 @@ package com.ssg.item.service;
 
 import com.ssg.item.dto.ItemDto;
 import com.ssg.item.dto.ItemResDto;
+import com.ssg.item.dto.ItemWithPromotionDto;
 import com.ssg.item.entity.Item;
+import com.ssg.item.entity.MapPromotionItem;
+import com.ssg.item.entity.Promotion;
 import com.ssg.item.entity.User;
 import com.ssg.item.enums.ItemType;
 import com.ssg.item.enums.UserStat;
@@ -26,6 +29,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -125,6 +129,114 @@ public class ItemServiceTest {
         assertThat(buyableItems2.size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 성공")
+    public void getItemWithPromotion() {
+        Item item = getStubItems().get(0);
+        int itemPrice = item.getItemPrice();
+        Promotion promotion = getStubPromotions().get(0);
+        setMapPromotionItem(item, promotion);
+
+        int discountAmount = promotion.getDiscountAmount() != null ? promotion.getDiscountAmount(): 0;
+        float discountRate = promotion.getDiscountRate() != null ? promotion.getDiscountRate(): 0;
+        int price0 = itemPrice - discountAmount;
+        int price1 = (int) (itemPrice * (1 - discountRate));
+        price0 = price0 > 0 ? price0 : itemPrice;
+        price1 = price1 > 0 ? price1 : itemPrice;
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getPromotionResDto()).isEqualTo(promotion.convertPromotionToResDto());
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(Math.min(price0, price1));
+    }
+
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 (프로모션이 없는 경우")
+    public void getItemWithNullPromotion() {
+        Item item = getStubItems().get(0);
+        int itemPrice = item.getItemPrice();
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(itemPrice);
+    }
+
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 (프로모션 할인이 너무 커 상품 가격이 0보다 적어질 때, 할인 적용 x)")
+    public void getItemWithTooMuchDiscount() {
+        Item item = getStubItems().get(0);
+        int itemPrice = item.getItemPrice();
+        Promotion promotion = getStubPromotions().get(2);
+        setMapPromotionItem(item, promotion);
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getPromotionResDto()).isEqualTo(null);
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(itemPrice);
+    }
+
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 (프로모션이 할인율과 할인비용을 동시에 가질 때)")
+    public void getItemAndPromotionWithDoubleDiscount() {
+        Item item = getStubItems().get(0);
+        int itemPrice = item.getItemPrice();
+        Promotion promotion = getStubPromotions().get(1);
+        setMapPromotionItem(item, promotion);
+
+        int discountAmount = promotion.getDiscountAmount() != null ? promotion.getDiscountAmount(): 0;
+        float discountRate = promotion.getDiscountRate() != null ? promotion.getDiscountRate(): 0;
+        int price0 = itemPrice - discountAmount;
+        int price1 = (int) (itemPrice * (1 - discountRate));
+        price0 = price0 > 0 ? price0 : itemPrice;
+        price1 = price1 > 0 ? price1 : itemPrice;
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getPromotionResDto()).isEqualTo(promotion.convertPromotionToResDto());
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(Math.min(price0, price1));
+    }
+
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 (프로모션 날짜가 해당 안됨)")
+    public void getItemAndPromotionWithWrongDate() {
+        Item item = getStubItems().get(0);
+        int itemPrice = item.getItemPrice();
+        Promotion promotion = getStubPromotions().get(3);
+        setMapPromotionItem(item, promotion);
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getPromotionResDto()).isEqualTo(null);
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(itemPrice);
+    }
+
+    @Test
+    @DisplayName("상품과 그에 해당하는 프로모션 조회 성공 (프로모션 수 복수)")
+    public void getItemWithPromotions() {
+        Item item = getStubItems().get(0);
+        Promotion promotion0 = getStubPromotions().get(0);
+        Promotion promotion1 = getStubPromotions().get(1);
+        setMapPromotionItem(item, promotion0);
+        setMapPromotionItem(item, promotion1);
+
+        given(itemRepository.findById(item.getId())).willReturn(Optional.of(item));
+
+        ItemWithPromotionDto itemWithPromotion = itemService.getItemWithPromotion(item.getId());
+        assertThat(itemWithPromotion.getPromotionResDto()).isEqualTo(promotion1.convertPromotionToResDto());
+        assertThat(itemWithPromotion.getItemDiscountDto().getDiscountedPrice()).isEqualTo(50);
+    }
+
+    private void setMapPromotionItem(Item item, Promotion promotion0) {
+        MapPromotionItem mapPromotionItem0 = new MapPromotionItem();
+        mapPromotionItem0.setPromotion(promotion0);
+        mapPromotionItem0.setItem(item);
+    }
+
     private List<Item> getStubItems() {
         Timestamp time0 = Timestamp.valueOf(LocalDateTime.now().withNano(0));
         Timestamp time1 = Timestamp.valueOf(LocalDateTime.now().plusMonths(1).withNano(0));
@@ -143,5 +255,21 @@ public class ItemServiceTest {
         users.add(new User(1, "name1", UserType.CORPORATE_USER, UserStat.WITHDRAWAL));
         users.add(new User(2, "name1", UserType.CORPORATE_USER, UserStat.NORMAL));
         return users;
+    }
+
+
+    private List<Promotion> getStubPromotions() {
+        Timestamp time0 = Timestamp.valueOf(LocalDateTime.now().withNano(0));
+        Timestamp time1 = Timestamp.valueOf(LocalDateTime.now().plusMonths(1).withNano(0));
+        Timestamp time2 = Timestamp.valueOf(LocalDateTime.now().plusMonths(2).withNano(0));
+        Timestamp time3 = Timestamp.valueOf(LocalDateTime.now().plusMonths(2).withNano(0));
+        Timestamp beforeNow = Timestamp.valueOf(LocalDateTime.now().minusMonths(1).withNano(0));
+
+        List<Promotion> promotions = new ArrayList<>();
+        promotions.add(new Promotion(0, "name0", null, 0.05f, time0, time1));
+        promotions.add(new Promotion(1, "name1", 1000, 0.5f, time0, time2));
+        promotions.add(new Promotion(2, "name2", 100000, null, time0, time3));
+        promotions.add(new Promotion(3, "name0", null, 0.05f, beforeNow, time0));
+        return promotions;
     }
 }
